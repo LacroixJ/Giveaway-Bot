500a8b79df8f2dab40417e6511146405bffd3c58
James <James2898@shaw.ca>
2018-09-30 18:19:54 -0700

    bot now adds the entry emoji for others to click

---
+++ config.py
@@ -0,0 +1,14 @@
+
+
+database = "tcgiveaway"
+hostname = "localhost"
+username = "TC"
+password = "tcbotpass"
+
+
+
+whitelist_roles = ["Server Owner","Community Manager","Admin","Developer"]
+whitelist_users = ["140943459593748480","133491626680123392","140204931528392705","118531063285940227"]
+#token = "NDkzNTI5OTIwNDg0NjcxNTAz.DomTHg.FY8gKMxBGt40YMdeZqrerBVjKwI"
+#token = "NDkzNTIzODUwNzMwOTMwMTc2.DomNpA.u1Ry-eZgd7PfwnA1qcDhMVztC4A"
+token = "NDgwMTIwMDc2MzA2MDg3OTU4.DnN0HQ.NiBABFRjrBOsSW7PVLmwJfSTzAI"                                                                             
